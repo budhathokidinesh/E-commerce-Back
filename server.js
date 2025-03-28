5d1b16f9ee2fc60bf5a4a388cff987ecac826dea
Dinesh Budhathoki <physmarika@gmail.com>
2025-03-28 12:07:35 +0800

    Basic server setup and databas is connected

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+import { dbConnect } from "./src/config/dbConfig.js";
+import { responseClient } from "./src/middlewares/responseClient.js";
+const app = express();
+const PORT = process.env.PORT || 8000;
+//middlewares
+import cookieparser from "cookie-parser";
+import cors from "cors";
+app.use(
+  cors({
+    origin: " http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieparser());
+app.use(express.json());
+
+//server status
+app.get("/", (req, res) => {
+  const message = "server is live";
+  responseClient({ req, res, message });
+});
+
+//db connection
+dbConnect().then(() => {
+  app.listen(PORT, (error) => {
+    error
+      ? console.log(error)
+      : console.log("Server is running at http://localhost:" + PORT);
+  });
+});
