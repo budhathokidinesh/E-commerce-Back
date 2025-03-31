7bf882f22679553186f71b268bba838f72f579ae
Dinesh Budhathoki <physmarika@gmail.com>
2025-03-31 17:34:30 +0800

    All endpoints for add products, edit products, fetch products and delete products done

--- src/controllers/admin/productsControllers.js
+++ src/controllers/admin/productsControllers.js
@@ -1,5 +1,7 @@
 import { imageUploadUtils } from "../../helpers/cloudinary.js";
+import Product from "../../models/ProductModel.js";
 
+//this controller for handling new image
 export const handleImageUpload = async (req, res) => {
   try {
     const b64 = Buffer.from(req.file.buffer).toString("base64");
@@ -12,6 +14,121 @@ export const handleImageUpload = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+//adding new products
+export const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      stock,
+    } = req.body;
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      stock,
+    });
+    await newlyCreatedProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newlyCreatedProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+//fetch all products
+export const fetchAllProducts = async (req, res) => {
+  try {
+    const listOfProducts = await Product.find({});
+    res.status(200).json({
+      success: true,
+      data: listOfProducts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+//edit a product
+export const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      stock,
+    } = req.body;
+    const findProduct = await Product.findById(id);
+    if (!findProduct)
+      return res.status(401).json({
+        success: false,
+        message: "Product not found",
+      });
+    findProduct.title = title || findProduct.title;
+    findProduct.description = description || findProduct.description;
+    findProduct.category = category || findProduct.category;
+    findProduct.brand = brand || findProduct.brand;
+    findProduct.price = price || findProduct.price;
+    findProduct.salePrice = salePrice || findProduct.salePrice;
+    findProduct.stock = stock || findProduct.stock;
+    findProduct.image = image || findProduct.image;
+    await findProduct.save();
+    res.status(200).json({
+      success: true,
+      data: findProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+//delete a product
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product)
+      return res.status(401).json({
+        success: false,
+        message: "Product not found",
+      });
+    res.json({
+      success: true,
+      message: "Product deleted successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
       success: false,
       message: "Error occured",
     });
--- src/routes/admin/productRoute.js
+++ src/routes/admin/productRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
 
-import { handleImageUpload } from "../../controllers/admin/productsControllers.js";
+import {
+  handleImageUpload,
+  addProduct,
+  editProduct,
+  deleteProduct,
+  fetchAllProducts,
+} from "../../controllers/admin/productsControllers.js";
 import { upload } from "../../helpers/cloudinary.js";
 
 const router = express.Router();
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/add", addProduct);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
+router.get("/get", fetchAllProducts);
 export default router;
