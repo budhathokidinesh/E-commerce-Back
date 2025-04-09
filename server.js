343f7533d3bff3cc16fa8cc577d511848ccc18ce
Dinesh Budhathoki <physmarika@gmail.com>
2025-04-09 17:58:04 +0800

    backend for reviews is finished

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 //end points
+//this is for authentication
 import authRoutes from "./src/routes/auth/authRoutes.js";
+
+//this is for admin endpoints
 import adminProductRoute from "./src/routes/admin/productRoute.js";
+import adminOrderRoute from "./src/routes/admin/orderRoute.js";
+
+//this is for shopping endpoints
 import shopProductRoute from "./src/routes/shopping/shopProductRoute.js";
 import shopCartRoute from "./src/routes/shopping/cartRoute.js";
 import shopAddressRoute from "./src/routes/shopping/addressRoute.js";
+import shopReviewRoute from "./src/routes/shopping/reviewRoute.js";
 import shopOrderRoute from "./src/routes/shopping/orderRoute.js";
 import shopSearchRoute from "./src/routes/shopping/searchRoute.js";
-import adminOrderRoute from "./src/routes/admin/orderRoute.js";
 
 //middlewares
 import cookieparser from "cookie-parser";
@@ -34,14 +40,20 @@ app.use(
 );
 app.use(cookieparser());
 app.use(express.json());
+//this is authentication
 app.use("/api/auth", authRoutes);
+
+//this is for admin
 app.use("/api/admin/products", adminProductRoute);
 app.use("/api/admin/orders", adminOrderRoute);
+
+//this is for shopping
 app.use("/api/shop/products", shopProductRoute);
 app.use("/api/shop/cart", shopCartRoute);
 app.use("/api/shop/address", shopAddressRoute);
 app.use("/api/shop/order", shopOrderRoute);
 app.use("/api/shop/search", shopSearchRoute);
+app.use("/api/shop/review", shopReviewRoute);
 
 //server status
 app.get("/", (req, res) => {
---
+++ src/controllers/shopping/productReviewController.js
@@ -0,0 +1,70 @@
+import Order from "../../models/Order.js";
+import Product from "../../models/ProductModel.js";
+import Review from "../../models/Reviews.js";
+
+//this is adding reviews
+export const addProductReview = async (req, res) => {
+  try {
+    const { productId, userId, userName, reviewMessage, reviewValue } =
+      req.body;
+    const order = await Order.findOne({
+      userId,
+      "cartItems.productId": productId,
+      orderStatus: "confirmed",
+    });
+    if (!order) {
+      return res.status(403).json({
+        success: false,
+        message: "You need to purchase this product to review it.",
+      });
+    }
+    const checkExistingReview = await Review.findOne({ productId, userId });
+    if (checkExistingReview) {
+      return res.status(400).json({
+        success: false,
+        message: "You already reviewed this product",
+      });
+    }
+    const newReview = new Review({
+      productId,
+      userId,
+      userName,
+      reviewMessage,
+      reviewValue,
+    });
+    await newReview.save();
+    const reviews = await Review.find({ productId });
+    const totalReviewsLength = reviews.length;
+    const averageReview =
+      reviews.reduce((sum, reviewItem) => sum + reviewItem.reviewValue, 0) /
+      totalReviewsLength;
+    await Product.findByIdAndUpdate(productId, { averageReview });
+    res.status(201).json({
+      success: true,
+      data: newReview,
+    });
+  } catch (error) {
+    console.log(first);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+//this is for getting reviews
+export const getProductReview = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const reviews = await Review.find({ productId });
+    res.status(200).json({
+      success: true,
+      data: reviews,
+    });
+  } catch (error) {
+    console.log(first);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
